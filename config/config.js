f4fc725a9ca32e70ab8bd959c6ca904929eb935a
Mahmoud Abdelsamie <48549895+MahmoudAbdelsamie@users.noreply.github.com>
2024-01-17 10:48:45 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@
 module.exports = {
     development : {
         username: "root",
-        password: "Ma17#Ma11@",
+        password: "jhjyh",
         database: "auth",
         host: "127.0.0.1",
         dialect: "mysql"
