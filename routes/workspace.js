b99521aeca1968a2ec3eb895e45a848981cc4f86
MahmoudAbdelsamie <msomaa38@gmail.com>
2024-01-23 15:08:22 +0200

    Create Space feature

--- routes/workspace.js
+++ routes/workspace.js
@@ -9,8 +9,18 @@ router.delete('/:id/delete', workspaceController.deleteWorkspace);
 router.get('/:id', workspaceController.getWorkspace);
 
 
-// Add Users to Workspace 
-router.post('/addUsers', workspaceController.addUsersToWorkspace);
+// Add Users to Workspace Route
+router.post('/:id/add-users', workspaceController.addUsersToWorkspace);
+
+
+// Space CRUD 
+
+router.post('/create', workspaceController.createSpace);
+router.get('/all', workspaceController.getSpaces);
+router.put('/:spaceId/update', workspaceController.updateSpace);
+router.delete('/:spaceId/update', workspaceController.deleteSpace);
+router.get('/:spaceId', workspaceController.getSpaceById);
+router.post('/:spaceId/add-users', workspaceController.addUsersToSpace);
 
 
 module.exports = router;
